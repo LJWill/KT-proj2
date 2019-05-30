df0d8ff9d68e3fa89778128d50615c7ccb1bd0e1
will li <javenli126@gmail.com>
2019-05-30 20:26:59 +1000

    code refactored

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def main(train_method):
         # clf_model.data_analysis(clf_model.eval_label[1])
         # clf_model.data_analysis(clf_model.train_label[1])
 
-    if train_method == 'nb':    clf_model.naive_bayes()
-    elif train_method == 'svm': clf_model.svm()
-    elif train_method == 'dt':  clf_model.decision_tree()
-    elif train_method == 'rf':  clf_model.random_forest()
+    if train_method == 'nb':     clf_model.naive_bayes()
+    elif train_method == 'svm':  clf_model.svm()
+    elif train_method == 'dt':   clf_model.decision_tree()
+    elif train_method == 'rf':   clf_model.random_forest()
     elif train_method == 'knn':  clf_model.knn()
     else: print('No methods found')
 
--- model.py
+++ model.py
@@ -12,7 +12,11 @@ from sklearn.metrics import classification_report
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import ensemble
 from sklearn import metrics
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 
+
+is_tunning_mode = True
+is_predicting_mode = False
 
 
 class Model:
@@ -21,68 +25,134 @@ class Model:
     train_label = pd.read_csv('./data/train-labels.txt', sep="\t", header=None)
     eval_set = pd.read_csv('./data/eval.csv')
     eval_label = pd.read_csv('./data/eval-labels.txt', sep="\t", header=None)
-    
-    def data_analysis(self, label_list):
-        no_positive = 0
-        no_negative = 0
-        no_neutral  = 0
 
-        for item in label_list:
-            if item == 'positive':
-                no_positive += 1
-            elif item == 'negative':
-                no_negative += 1
-            else:
-                no_neutral  += 1
-
-        print(no_positive, no_negative, no_neutral)
-
-        labels = 'positive', 'negative', 'neutral'
-        sizes = [no_positive, no_negative, no_neutral]
-        colors = ['gold', 'yellowgreen', 'lightcoral'] #, 'lightskyblue']
-        explode = (0.1, 0, 0)  # explode 1st slice
-        
-        plt.pie(sizes, explode=explode, labels=labels, colors=colors,
-                autopct='%1.2f%%', shadow=True, startangle=140)
-        
-        plt.axis('equal')
-        plt.show()
-
-
-    def naive_bayes(self):
-        clf_model = naive_bayes.MultinomialNB()
+    def naive_bayes(self, X=train_set, y=train_label[1]):
+        clf_model = naive_bayes.MultinomialNB(alpha=1.0, class_prior=None, fit_prior=True)
         # clf_model = naive_bayes.GaussianNB()
 
-        eval_pred = clf_model.fit(self.train_set, self.train_label[1]).predict(self.eval_set)
+        eval_pred = clf_model.fit(X, y).predict(self.eval_set)
 
         self.output_result(eval_pred)
 
-    def svm(self):
-        # clf_model = svm.SVC(kernel='linear')
-        clf_model = svm.SVC(kernel='rbf')
-        eval_pred = clf_model.fit(self.train_set, self.train_label[1]).predict(self.eval_set)
+    def svm(self, X=train_set, y=train_label[1]):
+        if is_tunning_mode:
+            cs = [0.001, 0.10, 0.1, 10, 25, 50, 100, 1000]
+            gammas = [0.01, 0.1, 1, 2, 3, 'auto']
+            param_grid = {
+                'kernel':('linear', 'rbf'), 
+                'C': cs,'gamma': gammas,
+                'decision_function_shape':('ovo','ovr'),
+                'shrinking':(True,False)
+            }
+            
+            grid = GridSearchCV(svm.SVC(C=1, verbose=True), param_grid, cv=10, scoring='accuracy')
+            grid.fit(X, y)
+            grid.grid_scores_
 
-        self.output_result(eval_pred)
+
+            print(grid.best_params_)
+            print(grid.best_score_)
+
+        else:
+            clf_model = svm.SVC()
+            eval_pred = clf_model.fit(X, y).predict(self.eval_set)
+            self.output_result(eval_pred)
 
 
-    def decision_tree(self):
-        clf_model = tree.DecisionTreeClassifier(random_state=0, max_depth=11)
-        eval_pred = clf_model.fit(self.train_set, self.train_label[1]).predict(self.eval_set)
+    def decision_tree(self, X=train_set, y=train_label[1]):
+        if is_tunning_mode:
+            # Maximum number of levels in tree
+            max_depth = list(range(3, 30))
+            # Minimum number of samples required to split a node
+            min_samples_split = [2, 5, 10]
+            # Minimum number of samples required at each leaf node
+            min_samples_leaf = [1, 2, 4]
+            criterion = ['gini','entropy']
 
-        self.output_result(eval_pred)
+            rand_param_grid = {
+                'max_depth': max_depth,
+                'min_samples_split': min_samples_split,
+                'min_samples_leaf': min_samples_leaf,
+                'criterion': criterion
+            }
+    
+            grid = RandomizedSearchCV(tree.DecisionTreeClassifier(random_state=0), rand_param_grid, 
+                   n_iter = 100, cv = 10, verbose=2, random_state=42, n_jobs = -1, scoring='accuracy')
+
+            grid.fit(X, y)
+
+            print(grid.best_params_)  # {'min_samples_split': 5, 'min_samples_leaf': 1, 'max_depth': 11, 'criterion': 'gini'}
+            print(grid.best_score_)   # 0.5470918345151607
+
+        else:
+            clf_model = tree.DecisionTreeClassifier(random_state=0, max_depth=11, min_samples_split=5, min_samples_leaf=1)
+            eval_pred = clf_model.fit(X, y).predict(self.eval_set)
+            self.output_result(eval_pred)
 
 
-    def random_forest(self):
-        clf_model =  RandomForestClassifier(n_estimators=100, criterion='gini', max_depth=15, random_state=0)
-        eval_pred = clf_model.fit(self.train_set, self.train_label[1]).predict(self.eval_set)
+    def random_forest(self, X=train_set, y=train_label[1]):
+        if is_tunning_mode:
+            # Number of trees in random forest
+            n_estimators = list(range(20, 200, 5))
+            # Maximum number of levels in tree
+            max_depth = list(range(3, 45))
+            # Minimum number of samples required to split a node
+            min_samples_split = [2, 5, 10]
+            # Minimum number of samples required at each leaf node
+            min_samples_leaf = [1, 2, 4]
+            # Method of selecting samples for training each tree
+            bootstrap = [True, False]
+            criterion = ['gini','entropy']
 
-        self.output_result(eval_pred)
+            rand_param_grid = {
+                'n_estimators': n_estimators,
+                'max_depth': max_depth,
+                'min_samples_split': min_samples_split,
+                'min_samples_leaf': min_samples_leaf,
+                'bootstrap': bootstrap,
+                'criterion': criterion
+            }
+    
+            grid = RandomizedSearchCV(RandomForestClassifier(random_state=0), rand_param_grid, 
+                   n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1, scoring='accuracy')
 
-    def knn(self):
-        clf_model = KNeighborsClassifier(n_neighbors = 3, algorithm="auto", leaf_size=30)
-        eval_pred = clf_model.fit(self.train_set, self.train_label[1]).predict(self.eval_set)
+            grid.fit(X, y)
 
-        self.output_result(eval_pred)
+            print(grid.best_params_)  
+            print(grid.best_score_)   
+
+        else:
+            clf_model =  RandomForestClassifier(n_estimators=100, criterion='gini', max_depth=15, random_state=0)
+            eval_pred = clf_model.fit(X, y).predict(self.eval_set)
+            self.output_result(eval_pred)
+
+
+
+    def knn(self, X=train_set, y=train_label[1]):
+        if is_tunning_mode:
+            n_neighbors = list(range(1,15))
+            weight_options = ['uniform','distance']
+            metric = ['euclidean', 'manhatan']
+            algorithm = ['auto', 'ball_tree', 'kd_tree', 'brute']
+
+            param_grid ={
+                'n_neighbors': n_neighbors,
+                'weights': weight_options,
+                'metric': metric,
+                'algorithm': algorithm
+            }
+
+            grid = GridSearchCV(KNeighborsClassifier(), param_grid, cv=3, scoring='accuracy', verbose=2)
+
+            grid.fit(X, y)
+
+            print(grid.best_params_)  
+            print(grid.best_score_)  
+
+        else:
+            clf_model = KNeighborsClassifier(n_neighbors = 3, algorithm="auto", leaf_size=30)
+            eval_pred = clf_model.fit(X, y).predict(self.eval_set)
+            self.output_result(eval_pred)
 
 
 
@@ -122,3 +192,30 @@ class Model:
         if self.is_id_match(self.eval_set['id'], self.eval_label[0]):
             self.eval_set.drop('id', axis = 1, inplace = True)
             self.eval_label.drop(0, axis = 1, inplace = True)
+
+
+    def data_analysis(self, label_list):
+        no_positive = 0
+        no_negative = 0
+        no_neutral  = 0
+
+        for item in label_list:
+            if item == 'positive':
+                no_positive += 1
+            elif item == 'negative':
+                no_negative += 1
+            else:
+                no_neutral  += 1
+
+        print(no_positive, no_negative, no_neutral)
+
+        labels = 'positive', 'negative', 'neutral'
+        sizes = [no_positive, no_negative, no_neutral]
+        colors = ['gold', 'yellowgreen', 'lightcoral'] #, 'lightskyblue']
+        explode = (0.1, 0, 0)  # explode 1st slice
+        
+        plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+                autopct='%1.2f%%', shadow=True, startangle=140)
+        
+        plt.axis('equal')
+        plt.show()
